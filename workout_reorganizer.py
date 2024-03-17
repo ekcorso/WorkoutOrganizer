986299ca921aa80ddc3cd0bc67cfa3a601ef03f0
Emily Corso <ekcorso@gmail.com>
2024-03-17 13:08:14 -0700

    Intial commit

---
+++ workout_reorganizer.py
@@ -0,0 +1,10 @@
+## Workout Reorganizer Script 
+## Takes a set of workouts in a google sheets folder and iterate over them to extract each workout into a separate spreadsheet.
+
+# Steps:
+# Enable the Google Sheets API
+# Fetch a list of files in the google sheets directory
+# Iterate over all files in the google sheets directory
+# For each file, iterate over each sheet
+# Copy each sheet into a new spreadhseet by itself and save it in the appropriate directory
+

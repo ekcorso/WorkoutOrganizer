7e5db3a207bd8e8948e89fa6caaffceb2e28d26c
Emily Corso <ekcorso@gmail.com>
2024-03-18 20:13:57 -0700

    Do basic setup

--- workout_reorganizer.py
+++ workout_reorganizer.py
@@ -1,4 +1,4 @@
-## Workout Reorganizer Script 
+## Workout Reorganizer Script
 ## Takes a set of workouts in a google sheets folder and iterate over them to extract each workout into a separate spreadsheet.
 
 # Steps:
@@ -16,3 +16,18 @@ Requirements:
 - Pip install the following: google-api-python-client, gspread
 # Confirm whether these are needed: google-auth-oauthlib, google-auth-oauthlib
 """
+
+
+import os
+import requests
+import gspread
+
+
+# First pass, try referencing a sheet with a known ID. Once I have enabled the Google Drive API as well I'll be able to fetch a list of the spreadsheet IDs that are in a given folder and work with each of them
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()

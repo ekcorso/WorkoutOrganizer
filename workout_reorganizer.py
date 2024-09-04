9a33aa87de2481990f6ab4d0a8342890796fbf8e
Emily Corso <ekcorso@gmail.com>
2024-09-04 12:58:15 -0700

    Run Black linter

--- workout_reorganizer.py
+++ workout_reorganizer.py
@@ -43,7 +43,11 @@ def fetch_list_of_files_in_folder(folder_id: str, client: Client) -> [Spreadshee
     spreadsheets_in_folder = client.list_spreadsheet_files(
         title=None, folder_id=folder_id
     )
-    valid_spreadsheets = list(filter(lambda n: "Workout Translations" not in n["name"], spreadsheets_in_folder))
+    valid_spreadsheets = list(
+        filter(
+            lambda n: "Workout Translations" not in n["name"], spreadsheets_in_folder
+        )
+    )
     return valid_spreadsheets
 
 
@@ -57,7 +61,10 @@ def create_new_spreadsheet(
 
 
 def separate_and_copy_all_sheets_to_folder(
-    spreadsheet: Spreadsheet, destination_folder_id: str, client: Client, translated_data: [SpreadsheetRow]
+    spreadsheet: Spreadsheet,
+    destination_folder_id: str,
+    client: Client,
+    translated_data: [SpreadsheetRow],
 ) -> None:
     """Copy all the sheets for spreadsheet and make each into a new spreadsheet in parallel"""
     sheets = spreadsheet.worksheets()
@@ -69,49 +76,84 @@ def separate_and_copy_all_sheets_to_folder(
             canary_cells = sheet.batch_get(["A1", "B2", "B4"])
             previous_description = get_workout_description(canary_cells)
             if is_valid_workout(canary_cells, previous_description):
-                futures.append(executor.submit(process_sheet, sheet, spreadsheet, destination_folder_id, client, previous_description, translated_data))
+                futures.append(
+                    executor.submit(
+                        process_sheet,
+                        sheet,
+                        spreadsheet,
+                        destination_folder_id,
+                        client,
+                        previous_description,
+                        translated_data,
+                    )
+                )
 
         for future in as_completed(futures):
             future.result()
 
 
-def process_sheet(sheet: Worksheet, spreadsheet: Spreadsheet, destination_folder_id: str, client: Client, previous_description: str, translated_data: [SpreadsheetRow]) -> None:
+def process_sheet(
+    sheet: Worksheet,
+    spreadsheet: Spreadsheet,
+    destination_folder_id: str,
+    client: Client,
+    previous_description: str,
+    translated_data: [SpreadsheetRow],
+) -> None:
     """Process a single sheet and copy it to a new spreadsheet in the destination folder"""
-    title = get_dest_spreadsheet_title(spreadsheet, previous_description, translated_data)
+    title = get_dest_spreadsheet_title(
+        spreadsheet, previous_description, translated_data
+    )
     dest_spreadsheet = create_new_spreadsheet(title, destination_folder_id, client)
     new_worksheet = sheet.copy_to(dest_spreadsheet.id)
-    dest_spreadsheet.get_worksheet_by_id(new_worksheet["sheetId"]).update_title("Workout")
+    dest_spreadsheet.get_worksheet_by_id(new_worksheet["sheetId"]).update_title(
+        "Workout"
+    )
     dest_spreadsheet.del_worksheet(dest_spreadsheet.sheet1)
 
 
-def should_process_spreadsheet(spreadsheet: Spreadsheet, translations: [SpreadsheetRow]) -> bool:
+def should_process_spreadsheet(
+    spreadsheet: Spreadsheet, translations: [SpreadsheetRow]
+) -> bool:
     """Check if translation sheet indicates that the workout should be skipped"""
     for translation in translations:
         if translation.original_name == spreadsheet.title:
             return not translation.skip
 
 
-def is_valid_workout(canary_cells: list[list[list[str]]], previous_description: str) -> bool:
+def is_valid_workout(
+    canary_cells: list[list[list[str]]], previous_description: str
+) -> bool:
     """Check that the worksheet is not a blank workout template"""
-    canary_cell_value = "" # A1
+    canary_cell_value = ""  # A1
 
     if isinstance(canary_cells[0], list) and canary_cells[0]:
-        canary_cell_value = canary_cells[0][0][0] if isinstance(canary_cells[0][0][0], str) else ""
+        canary_cell_value = (
+            canary_cells[0][0][0] if isinstance(canary_cells[0][0][0], str) else ""
+        )
     else:
         canary_cell_value = ""
 
     completely_blank = not any(flatten_3d_list(canary_cells))
     is_foundation_workout = "Foundation 1" in previous_description
-    is_valid = False if (canary_cell_value == "Name: " or is_foundation_workout or completely_blank) else True 
+    is_valid = (
+        False
+        if (canary_cell_value == "Name: " or is_foundation_workout or completely_blank)
+        else True
+    )
     return is_valid
 
 
 def flatten_3d_list(data: list[list[list[str]]]) -> [str]:
-   """Flatten a 3D list"""
-   return [item for sublist1 in data for sublist2 in sublist1 for item in sublist2]
+    """Flatten a 3D list"""
+    return [item for sublist1 in data for sublist2 in sublist1 for item in sublist2]
 
 
-def get_dest_spreadsheet_title(spreadsheet: Spreadsheet, previous_description: str, translated_data: [SpreadsheetRow]) -> str:
+def get_dest_spreadsheet_title(
+    spreadsheet: Spreadsheet,
+    previous_description: str,
+    translated_data: [SpreadsheetRow],
+) -> str:
     """Create and return a title for the new spreadsheet"""
     source_title = spreadsheet.title
     translated_source_title = translate_workout_name(source_title, translated_data)
@@ -121,7 +163,14 @@ def get_dest_spreadsheet_title(spreadsheet: Spreadsheet, previous_description: s
 
 def translate_workout_name(source_title: str, translated_data: [SpreadsheetRow]) -> str:
     """Translate the workout title from the source title to a supplied description"""
-    translated_name = next((item.description for item in translated_data if item.original_name == source_title), None)
+    translated_name = next(
+        (
+            item.description
+            for item in translated_data
+            if item.original_name == source_title
+        ),
+        None,
+    )
     return str(translated_name) if translated_name else str(source_title)
 
 
@@ -133,9 +182,11 @@ def get_client_name_list_from_spreadsheets(spreadsheets: [Spreadsheet]) -> [str]
 def create_workout_translation_spreadsheet(
     dest_folder_id: str, client: Client, current_client_files: [Spreadsheet]
 ) -> Spreadsheet:
-    """Create a new spreadsheet in the destination folder with the title 'Workout Translation' and return the new spreadsheet
+    """Create a new spreadsheet in the destination folder with the title 'Workout Translations' and return the new spreadsheet
     with a list of all the client names in the origin folder."""
-    spreadsheet = create_new_spreadsheet("Workout Translations", dest_folder_id, client)
+    spreadsheet = create_new_spreadsheet(
+        "Workout Translations", dest_folder_id, client
+    )  # Create in dest folder so that the client's original folder is never modified
     names = get_client_name_list_from_spreadsheets(current_client_files)
     sheet = spreadsheet.sheet1
     all_rows = [["Original Name", "Description", "Skip?"]] + names
@@ -145,10 +196,12 @@ def create_workout_translation_spreadsheet(
 
 def get_workout_description(canary_cells: list[list[list[str]]]) -> str:
     """Return the description of the workout from the spreadsheet"""
-    canary_cell_location = 0 # A1: this cell will be blank if the workout is an older format
-    new_description_location = 1 # B2
-    old_description_location = 2 # B4
-    
+    canary_cell_location = (
+        0  # A1: this cell will be blank if the workout is an older format
+    )
+    new_description_location = 1  # B2
+    old_description_location = 2  # B4
+
     canary_cell_value = get_value_at_location(canary_cells, canary_cell_location)
     newer_description = get_value_at_location(canary_cells, new_description_location)
     older_description = get_value_at_location(canary_cells, old_description_location)

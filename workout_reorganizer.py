c5dfceb23255dc2dd183fe636f73674fa30f4016
Emily Corso <ekcorso@gmail.com>
2024-09-08 18:10:38 -0700

    Fix SpreadsheetRow init param types

--- workout_reorganizer.py
+++ workout_reorganizer.py
@@ -23,7 +23,7 @@ class SpreadsheetRow:
     description: str
     skip: bool
 
-    def __init__(self, original_name: str, description: str, skip: bool) -> None:
+    def __init__(self, original_name: str, description: str, skip: str) -> None:
         self.original_name = original_name
         self.description = description
         self.skip = self.should_skip(skip)
